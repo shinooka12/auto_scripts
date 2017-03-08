551a024448d5d99ec40915b85e7fae3b5a769d16
shinooka12 <fps_only_dayo072@yahoo.co.jp>
2017-03-08 20:57:31 +0900

    abc
    abc

--- auto_login.rb
+++ auto_login.rb
@@ -5,9 +5,9 @@ driver.get "URL"
 sleep 1
 
 #element.clear
-element = driver.find_element(:id, "userid")
+element = driver.find_element(:id, "username field id")
 element.send_keys("username");
-element = driver.find_element(:id, "userpwd")
+element = driver.find_element(:id, "userpwd field id")
 element.send_keys("passward");
 sleep 1
 element = driver.find_element(:link, "LOG IN").click
